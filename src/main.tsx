372c17851ebe756e73b8f9f8611ae916a292c0e7
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-05-16 20:18:05 +0300

    added redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App.tsx";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import recipeReducer from './features/recipeSlice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { recipesApi } from './features/recipesService';
 
-export const store = configureStore({
-    reducer: {
-        recipes: recipeReducer,
-        [recipesApi.reducerPath]: recipesApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(recipesApi.middleware)
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const rootReducer = combineReducers({
+    recipes: recipeReducer,
+    [recipesApi.reducerPath]: recipesApi.reducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: [recipesApi.reducerPath]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(recipesApi.middleware),
 });
 
+export const persistor = persistStore(store)
+export default store
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
