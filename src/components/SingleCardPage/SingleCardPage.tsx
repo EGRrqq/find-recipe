ae7befd224c14a4deb7d5b4d0f5c515f9ee99eef
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-05-16 19:41:10 +0300

    Improved single recipe page

--- src/components/SingleCardPage/SingleCardPage.tsx
+++ src/components/SingleCardPage/SingleCardPage.tsx
@@ -1,9 +1,28 @@
-import { Center } from "@chakra-ui/react"
-import { useParams } from "react-router-dom"
-import { useGetSingleRecipeQuery } from "../../services/recipesService";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Heading,
+  Image,
+  Spinner,
+  Stack,
+  Text,
+  VStack,
+  Link,
+  useColorMode,
+} from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { useGetSingleRecipeQuery } from "../../store/features/recipesService";
+import { AnimatePresence, motion } from "framer-motion";
 
 const SingleCardPage: React.FC = () => {
-  const { id } = useParams()
+  const { id } = useParams();
+  const { colorMode } = useColorMode();
+
   const {
     data: card,
     isLoading,
@@ -12,10 +31,66 @@ const SingleCardPage: React.FC = () => {
   } = useGetSingleRecipeQuery(String(id));
 
   return (
-    <Center>
-      {card?.recipe.label}
-    </Center>
-  )
-}
+    <>
+      {isError && (
+        <Center>
+          <Alert status="error">
+            <AlertIcon />
+            {cardError.error}
+          </Alert>
+        </Center>
+      )}
 
-export default SingleCardPage
+      <VStack align="stretch" px="16">
+        <AnimatePresence>
+          <Card
+            as={motion.div}
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+          >
+            {isLoading ? (
+              <Center>
+                <Spinner
+                  thickness="4px"
+                  speed="0.65s"
+                  emptyColor="gray.200"
+                  color="blue.500"
+                  size="xl"
+                />
+              </Center>
+            ) : (
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src={card?.recipe.image}
+                alt="recipe image"
+              />
+            )}
+
+            <Stack>
+              <CardBody>
+                <Heading size="md">{card?.recipe.label}</Heading>
+
+                <Text py="2">
+                  See full recipe on:{" "}
+                  <Link href={card?.recipe.url} color={colorMode === 'light' ? 'green.500' : 'teal.200'} isExternal>{card?.recipe.source} </Link>
+                </Text>
+              </CardBody>
+
+              <CardFooter>
+                <Button variant="solid" colorScheme="teal">
+                  Save
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </AnimatePresence>
+      </VStack>
+    </>
+  );
+};
+
+export default SingleCardPage;
